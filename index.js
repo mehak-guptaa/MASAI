ea688ed4fbe134192f6e58f329bc2e89c3112b4d
mehak-guptaa <parthgupta75715@gmail.com>
2024-12-10 21:20:26 +0530

    navbar

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ let a = 5;
 let b = 6;
 let c = 1;
 console.log(a + b + c)
+
+//  to create a nwe branch we do the following steps:
+//  1. git branch branch-name
+//  2. git checkout branch-name (to get switched to branch )
+//  3. smple follow A.C.P = add . , commit, push origin branch -name
